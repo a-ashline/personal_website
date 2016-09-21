72653b9f1a86a6430b80b625c22bb6ac69d80a73
a-ashline <ashline2012@yahoo.com>
2016-09-20 23:26:02 -0400

    edit page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 11:19 PM
  */
 
-echo '<div style="center">
+echo '<div style="center">hello
             <embed src="Resume.6.6.2016.pdf" width="800px" height="2100px" />
         </div>
            ';
