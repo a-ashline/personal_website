3da46a7de0459e993b81f9ec7edc4670bb6c5313
a-ashline <ashline2012@yahoo.com>
2016-09-20 23:48:40 -0400

    s

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
  * Time: 11:19 PM
  */
 
-echo '<div style="center">
-<h2>hello</h2>
+echo '<div style="text-align:center">
+<h2>Hello and welcome to my website.</h2>
+ <p>It is currently being developed on as you read this. Thank you for your time and patience as I continue to improve this site.</p>
             <embed src="Resume.6.6.2016.pdf" width="800px" height="2100px" />
         </div>
            ';
